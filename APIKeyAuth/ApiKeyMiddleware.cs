a20b54c97849d3ad38c5dc4bdfa77528d12c1786
AlishaMaljapte <alishamaljapte@stspl.com>
2023-11-20 12:18:23 +0530

    My first commit

---
+++ APIKeyAuth/ApiKeyMiddleware.cs
@@ -0,0 +1,31 @@
+﻿namespace APIKeyAuth
+{
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;  //This is an instance of RequestDelegate
+        private const string APIKEY = "XApiKey"; //This constant string represents the name of the API key in the HTTP headers. In this case, it's set to "XApiKey."
+        public ApiKeyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(APIKEY, out
+                    var extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Api Key was not provided ");
+                return;
+            }
+            var appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
+            var apiKey = appSettings.GetValue<string>(APIKEY);
+            if (!apiKey.Equals(extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorized client");
+                return;
+            }
+            await _next(context); //If the API key is successfully validated, the middleware calls the next middleware in the pipeline.
+        }
+    }
+}
